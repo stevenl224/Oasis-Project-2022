c429fa2548b64bc99dba0494cebe6ddd9c66ef93
ethankong150 <kong.e@northeastern.edu>
2022-10-09 13:19:43 -0400

    login page with implementation into App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import LoginPage from './LoginPage.js'
+import './stylesheets/app.css';
 
 // This is a small change
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoginPage/>
     </div>
   );
 }
