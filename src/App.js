276971c7fc13e5514bde7ef182e110d9efe003a2
ethankong150 <kong.e@northeastern.edu>
2022-10-30 15:25:11 -0400

    revamped navigation system + createAccountPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import LoginPage from './pages/LoginPage.js'
-import './stylesheets/app.css';
+import LandingPage from './pages/LandingPage.js'
+import CreateAccountPage from './pages/CreateAccountPage'
+import { useState } from 'react';
+
 
 // This is a small change
 function App() {
-  return (
-    <div className="App">
-      <LoginPage/>
-    </div>
-  );
+
+  const[navigation, setNavigation] = useState("loginPage");
+
+  const[database , setDatabase] = useState([ {username: 'admin', password: '123'}]);
+  
+
+  
+if (navigation === "loginPage") {
+  return <LoginPage navigation={navigation} setNavigation={setNavigation} database={database} setDatabase={setDatabase}/>
+} else if(navigation === "createAccount") {
+  return <CreateAccountPage navigation={navigation} setNavigation={setNavigation} database={database} setDatabase={setDatabase}/>
+} else if(navigation === "landingPage") {
+  return <LandingPage navigation={navigation} setNavigation={setNavigation}/>
+}
+  
 }
 
 export default App;
