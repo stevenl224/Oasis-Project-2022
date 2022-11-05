16d03a2f371cb8763ee9efd3b419e338a2a39b2d
ethankong150 <kong.e@northeastern.edu>
2022-11-05 17:32:42 -0400

    fix

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,27 +3,34 @@ import { useState } from "react";
 import DataTable from "../components/datatable.js"
 import PieChart from "../components/PieChart.js"
 import MoneySpent from "../components/MoneySpent.js"
-import LinearDeterminate from "../components/LinearDeterminate.js"
 import {TRANSACTIONS} from "../data/data.js"
 
 
 function LandingPage(props) {
 
-  const[spentSoFar, setSpentSoFar] = useState(40);
+  const[spentSoFar, setSpentSoFar] = useState(200);
   const[totalToSpend, setTotalToSpend] = useState(400);
 
+      //  <div className = 'transactions'> </div>
     return ( 
-    <div> 
-        <MoneySpent spentSoFar={spentSoFar}totalToSpend={totalToSpend}/>
-      <div>
-        <h1>Hi</h1>
-      </div>
-    <DataTable transactions = {TRANSACTIONS}/> 
-    <PieChart/> 
-    <LinearDeterminate spentSoFar={spentSoFar}totalToSpend={totalToSpend}/>
-    </div>
+      <body>
+        <div className='greeting'>
+          <h1>Hello, user.</h1>
+          <h2>Here are your transactions for the day.</h2>
+        </div>
+         <div className = 'transactions'> 
+          <DataTable transactions = {TRANSACTIONS}/> 
+       </div>
+        <div className = 'piechart'>       
+          <PieChart/> 
+        </div>
+        <div className = 'progress'><MoneySpent spentSoFar={spentSoFar}
+          setSpentSoFar={setSpentSoFar} 
+          totalToSpend={totalToSpend}
+          setTotalToSpend={setTotalToSpend}/>
+        </div>
+        </body>
     )
 }
 
 export default LandingPage;
-
